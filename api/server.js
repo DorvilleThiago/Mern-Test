d3fad1d37b27dc339faf23fc0b0fbb5942338969
DorvilleThiago <thiago.dorville@souunit.com.br>
2023-04-02 08:33:14 -0300

    update

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jsonwebtoken = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 const app = express();
+const JWT_SECRET = "86xlVV^1A2y&NZCC7Vb0&fh&I4j45GD8KT7tOXTdQ8svxLGrt#"
 
 app.use(express.json());
 app.use(cors());
@@ -11,6 +14,60 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("Connected to Mongo")).catch(console.error);
+
+// AUTH
+
+const User = require('./models/User')
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    console.log(`${email} is trying to login ..`);
+
+    User.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(password, user.password)
+                    .then(result => {
+                        if (result) {
+                            return res.json({token: jsonwebtoken.sign({ user: email }, JWT_SECRET)})
+                        } else {
+                            res.json({ error: "password incorrect" })
+                        }
+                    });
+            } else {
+                console.log('User not found');
+            }
+        })
+        .catch(err => {
+            throw err;
+        });
+})
+
+app.post("/registro", (req, res) => {
+    const { email, password } = req.body;
+    User.findOne({ email: email }).then(user => {
+        if (user) {
+            res.json({ error: "user already exists" })
+        } else {
+            const saltRounds = 10;
+            bcrypt.hash(password, saltRounds, function (err, hashedPassword) {
+                const user = new User({
+                    email: email,
+                    password: hashedPassword
+                })
+                user.save();
+                return res.json({token: jsonwebtoken.sign({ user: email }, JWT_SECRET)})
+            })
+        }
+    })
+})
+
+app.get('/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users)
+})
+
+// TODO
 
 const Todo = require('./models/Todo')
 
@@ -38,5 +95,7 @@ app.get('/todo/complete/:id', async (req, res) => {
     todo.save();
     res.json(todo);
 })
+
+// LISTEN
 
 app.listen(3001, () => console.log("Server started on port 3001"))
